ca1ee2ed271efed72a3b5a9ad525c320b4e31997
wilmarmarin <will-marin@hotmail.es>
2023-08-03 00:20:01 -0500

    nivel 1

--- Prueba 3/Assets/scrips/QuitManager.cs
+++ Prueba 3/Assets/scrips/QuitManager.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class QuitManager : MonoBehaviour
 {
- //   public void Exit()
- //   {
- //       UnityEditor.EditorApplication.isPlaying = false;
- //       Application.Quit();
- //   }
+    public void Exit()
+    {
+        //UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
 }
